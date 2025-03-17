d0c5b5d097a8c534ed7ffc13493654d47b0d1c1a
Duc Thanh Nguyen <ngtduc693@gmail.com>
2025-03-17 18:01:38 +0700

    Create the gRPC + .NET 10 example

---
+++ Example.gRPC/Program.cs
@@ -0,0 +1,12 @@
+﻿using Example.gRPC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<EmotionDetectorService>();
+app.MapGet("/", () => "gRPC service running...");
+
+app.Run();
