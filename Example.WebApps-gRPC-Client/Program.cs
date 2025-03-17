a531b4395e522c10abb86457df34a19ec8a53654
Duc Nguyen <ngtduc693@gmail.com>
2025-03-17 22:12:14 +0700

    Add the gRPC Client

--- Example.WebApps-gRPC-Client/Program.cs
+++ Example.WebApps-gRPC-Client/Program.cs
@@ -1,3 +1,5 @@
+using Example.gRPC;
+
 namespace Example.WebApps
 {
     public class Program
@@ -7,6 +9,12 @@ namespace Example.WebApps
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllersWithViews();
+            builder.Services.AddGrpcClient<EmotionDetector.EmotionDetectorClient>(o =>
+            {
+                // add the gRPC server here, you can build it from Example.gRPC project in this solution
+                // or you can create the container from the image from the docker hub by "docker pull ngtduc693/ai.facial.emotion-net10-grpc:lastest"
+                o.Address = new Uri("http://localhost:5000/");
+            });
 
             var app = builder.Build();
 
